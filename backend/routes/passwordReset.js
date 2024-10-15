fe9f94c48576afdd9e23c5196689c8c68a4d23be
sumit <sumitkumarbhagat987@gmail.com>
2024-10-15 21:59:04 +0530

    forgot-password

--- backend/routes/passwordReset.js
+++ backend/routes/passwordReset.js
@@ -39,7 +39,7 @@ router.post("/forgot-password", async (req, res) => {
       subject: "Password Reset",
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
                    Please click on the following link, or paste this into your browser to complete the process:\n\n
-                   http://localhost:5173/reset-password/${resetToken}\n\n
+                   https://bookify-hub.vercel.app/reset-password/${resetToken}\n\n
                    If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
